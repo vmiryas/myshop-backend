e4abc7990a64cccdc4101781acb2bae9916d6842
Volodymyr Yas <volodymyryas@vryas.local>
2023-07-29 21:02:56 +0300

    feat(actions-two)

---
+++ src/handlers/carts/carts.spec.ts
@@ -0,0 +1,9 @@
+describe('CartsController', () => {
+  describe('findAll', () => {
+    it('should return true', async () => {
+      const result = 1
+
+      expect(1).toBe(result);
+    });
+  });
+});
