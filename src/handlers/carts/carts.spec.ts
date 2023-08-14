9af10f37601e7e079de4324be2a200339058e0a2
Volodymyr Yas <volodymyryas@vryas.local>
2023-08-14 16:53:33 +0300

    added docker file and updated actions script

--- src/handlers/carts/carts.spec.ts
+++ src/handlers/carts/carts.spec.ts
@@ -1,7 +1,7 @@
 describe('CartsController', () => {
   describe('findAll', () => {
     it('should return true', async () => {
-      const result = 1
+      const result = 1;
 
       expect(1).toBe(result);
     });
